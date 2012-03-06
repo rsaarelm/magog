efa17d54dc47ff79829c6adf863f71438e3dab83
Risto Saarelma <risto.saarelma@iki.fi>
2012-03-06 16:08:34 +0200

    Dropped the unused dependency to a Boost library for the moment

--- ui/main.cpp
+++ ui/main.cpp
@@ -3,11 +3,9 @@
 #include <util.hpp>
 #include <util/winmain.hpp>
 #include "intro_screen.hpp"
-#include "config.hpp"
 
 int main(int argc, char* argv[])
 {
-  parse_command_line(argc, argv);
   Game_Loop& game = Game_Loop::init(800, 600, "Telos");
 
   init_font(_fontdata_font);
