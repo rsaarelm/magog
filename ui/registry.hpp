6b26eba54a5bec5e844e06a6286075b0d5ca8770
Risto Saarelma <risto.saarelma@iki.fi>
2012-03-11 12:35:06 +0200

    Add Registry class to hold global application settings

---
+++ ui/registry.hpp
@@ -0,0 +1,28 @@
+/* registry.hpp
+
+   Copyright (C) 2012 Risto Saarelma
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef UI_REGISTRY_HPP
+#define UI_REGISTRY_HPP
+
+/// Application-wide configuration settings.
+struct Registry {
+  static int window_w;
+  static int window_h;
+  static bool using_colemak;
+};
+
+#endif
