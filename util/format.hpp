107922b95ed616934f1a608a3b50682b984cc32e
Risto Saarelma <risto.saarelma@iki.fi>
2012-03-19 18:52:15 +0200

    Variadic string formatter using the operator<< idiom to stringify values

---
+++ util/format.hpp
@@ -0,0 +1,49 @@
+/* format.hpp
+
+   Copyright (C) 2012 Risto Saarelma
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef UTIL_FORMAT_HPP
+#define UTIL_FORMAT_HPP
+
+/// \file format.hpp \brief C++-style variadic printf-style string construction.
+
+#include <string>
+#include <sstream>
+
+class Format_Exception : public std::exception {};
+
+std::string format(const char* fmt);
+
+template<typename T, typename... Args>
+std::string format(const char* fmt, T value, Args... args) {
+  std::stringstream result;
+
+  while (*fmt) {
+    if (*fmt == '%' && *(++fmt) != '%') {
+      if (*fmt++ != 's')
+        throw "format only supports %s";
+      result << value;
+      result << format(fmt, args...);
+      return result.str();
+    } else {
+      result << *fmt++;
+    }
+  }
+
+  throw "extra arguments given to format";
+}
+
+#endif
