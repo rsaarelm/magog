25f5dbd548f046ba91ed12d6923cc869b1976851
Risto Saarelma <risto.saarelma@iki.fi>
2014-01-25 13:29:05 +0200

    Utils junk

--- src/crunchy/main.rs
+++ src/crunchy/main.rs
@@ -1,6 +1,21 @@
 extern mod calx;
+use calx::text;
+use std::num::log10;
+use std::rand::Rng;
+use std::rand;
+
+pub fn probToDb(prob: f64) -> f64 {
+    assert!(prob > 0.0);
+    assert!(prob < 1.0);
+    -10.0 * log10(1.0 / prob - 1.0)
+}
+
+pub fn randomDb<R: Rng>(rng: &mut R) -> f64 {
+    probToDb(rng.gen_range(1.0, 99.0) / 100.0)
+}
 
 pub fn main() {
-    calx::hello();
-    println!("Crunchy: A program for testing rules and object interactions.");
+    println!("{}\n",
+	text::wrap_lines(20, "Crunchy: A program for testing rules and object interactions."));
+    println!("{}\n", randomDb(&mut rand::task_rng()));
 }
--- src/shiny/main.rs
+++ src/shiny/main.rs
@@ -5,6 +5,36 @@ pub fn main() {
     calx::hello();
     sdl2::init([sdl2::InitVideo]);
     println!("Shiny: A prototype user interface.");
-    // TODO: Link to SDL or sth.
+    let window = match sdl2::video::Window::new("Shiny!",
+        sdl2::video::PosCentered, sdl2::video::PosCentered,
+        800, 600, [sdl2::video::OpenGL]) {
+        Ok(window) => window,
+        Err(err) => fail!(format!("SDL2 window fail: {}", err))
+    };
+
+    let renderer = match sdl2::render::Renderer::from_window(window, sdl2::render::DriverAuto, [sdl2::render::Accelerated]) {
+        Ok(renderer) => renderer,
+        Err(err) => fail!(format!("SDL2 renderer fail: {}", err))
+    };
+
+    renderer.set_draw_color(sdl2::pixels::RGB(16, 0, 0));
+    renderer.clear();
+    renderer.present();
+
+    loop {
+        match sdl2::event::poll_event() {
+            sdl2::event::QuitEvent(_) => break,
+            sdl2::event::KeyDownEvent(_, _, key, _, _) => {
+                if key == sdl2::keycode::EscapeKey {
+                    break
+                }
+            },
+            sdl2::event::WindowEvent(_, _, _, _, _) => {
+                renderer.clear();
+                renderer.present();
+            }
+            _ => {}
+        }
+    }
 }
 
