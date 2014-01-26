1511d559ed4e3e2b9c92986f18ca08c1ea9b07fb
Risto Saarelma <risto.saarelma@iki.fi>
2014-01-26 13:02:52 +0200

    Try to spec crates as rlibs (rustpkg still dynlinks, not working)

--- src/calx/lib.rs
+++ src/calx/lib.rs
@@ -1,6 +1,8 @@
+#[crate_id = "calx"];
 #[desc = "Shared gamelib"];
 #[license = "GPLv3"];
 #[feature(globs)];
+#[crate_type = "rlib"];
 
 extern mod sdl2;
 extern mod extra;
--- src/stb/lib.rs
+++ src/stb/lib.rs
@@ -1,5 +1,7 @@
+#[crate_id = "stb"];
 #[desc = "Static STB library integration"];
 #[license = "MIT"];
+#[crate_type = "rlib"];
 
 #[feature(link_args)];
 #[feature(globs)];
