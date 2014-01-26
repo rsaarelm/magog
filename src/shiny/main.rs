d9cef012674a77a3d45535a66c4f837400588afc
Risto Saarelma <risto.saarelma@iki.fi>
2014-01-26 21:54:30 +0200

    Demo uses OpenGL drawing

--- src/shiny/main.rs
+++ src/shiny/main.rs
@@ -1,6 +1,9 @@
 extern mod stb;
 
 extern mod glfw;
+extern mod opengles;
+
+use opengles::gl2;
 
 //use std::io::File;
 
@@ -10,6 +13,14 @@ pub fn main() {
         let window = glfw::Window::create(800, 600, "Shiny!", glfw::Windowed)
             .expect("Failed to create window.");
         window.make_context_current();
+
+	gl2::viewport(0, 0, 800, 600);
+	gl2::clear_color(0.0, 0.8, 0.8, 1.0);
+	gl2::clear(gl2::COLOR_BUFFER_BIT | gl2::DEPTH_BUFFER_BIT);
+	gl2::flush();
+
+	window.swap_buffers();
+
         while !window.should_close() {
             glfw::poll_events();
         }
