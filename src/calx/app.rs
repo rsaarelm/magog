005d102dd739a188b99f514c92a1d684e7a58da1
Risto Saarelma <risto.saarelma@iki.fi>
2014-01-26 12:07:47 +0200

    Pixel perfect window resize in SDL2 app

--- src/calx/app.rs
+++ src/calx/app.rs
@@ -5,15 +5,20 @@ use std::vec;
 use sdl2::pixels;
 use sdl2::render;
 use sdl2::video;
+use sdl2::rect::Rect;
 
 pub struct App {
     priv renderer: ~render::Renderer,
     priv texture: ~render::Texture,
-
-    pixels: ~[u8],
     priv width: uint,
     priv height: uint,
-}
+
+    // Screen buffer to write to.
+    pixels: ~[u8],
+
+    // When true, making the window bigger will only make the pixels grow in
+    // integer multiples. Keeps pixel art pretty.
+    pixelPerfect: bool, }
 
 impl App {
     pub fn new(title: ~str, width: uint, height: uint) -> Result<~App, ~str> {
@@ -21,12 +26,12 @@ impl App {
 
         let window = match video::Window::new(title,
             sdl2::video::PosCentered, sdl2::video::PosCentered,
-            width as int, height as int, [video::OpenGL]) {
+            width as int, height as int, [video::Shown, video::Resizable]) {
             Ok(window) => window,
             Err(err) => return Err(format!("SDL2 window fail: {}", err))
         };
         let renderer = match render::Renderer::from_window(window,
-            render::DriverAuto, [render::Accelerated]) {
+            render::DriverAuto, [render::Accelerated, render::PresentVSync]) {
             Ok(renderer) => renderer,
             Err(err) => return Err(format!("SDL2 renderer fail: {}", err))
         };
@@ -39,17 +44,43 @@ impl App {
         Ok(~App{
             renderer: renderer,
             texture: texture,
-            pixels: pixels,
             width: width,
             height: height,
+            pixels: pixels,
+            pixelPerfect: true,
         })
+    }
+
+    fn targetRect(&self) -> Rect {
+        let port = self.renderer.get_viewport();
+
+        let (w, h) = targetSize(self.width, self.height, &port, self.pixelPerfect);
+        Rect{
+            x: ((port.w as uint - w) / 2) as i32, y: ((port.h as uint - h) / 2) as i32,
+            w: w as i32, h: h as i32
+        }
     }
 
     pub fn render(&self) {
         self.renderer.clear();
         self.texture.update(None, self.pixels, self.width as int * 4);
-        self.renderer.copy(self.texture, None, None);
+        self.renderer.copy(self.texture, None, Some(self.targetRect()));
         self.renderer.present();
     }
 }
 
+fn targetSize(width: uint, height: uint, port: &Rect, pixelPerfect: bool) -> (uint, uint) {
+    let wScale = port.w as f64 / width as f64;
+    let hScale = port.h as f64 / height as f64;
+    let minScale = if wScale < hScale { wScale } else { hScale };
+
+    if minScale < 1.0 || !pixelPerfect {
+         // Less than 1 physical pixel for 1 logical pixel, can't achieve
+         // pixel perfection so just scale to what we get.
+        ((width as f64 * minScale) as uint,
+         (height as f64 * minScale) as uint)
+    } else {
+        ((width as f64 * minScale.floor()) as uint,
+         (height as f64 * minScale.floor()) as uint)
+    }
+}
