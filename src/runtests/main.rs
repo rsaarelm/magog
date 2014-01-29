6f54c24ac5e386d2b88310bc1e7e051d6d4d9ed7
Risto Saarelma <risto.saarelma@iki.fi>
2014-01-30 00:45:14 +0200

    Building and running unit tests

---
+++ src/runtests/main.rs
@@ -0,0 +1,22 @@
+use std::io::fs;
+use std::io;
+use std::os;
+use std::run::{Process, ProcessOptions};
+
+pub fn main() {
+    for i in fs::readdir(&os::getcwd()).iter() {
+        let stat = i.stat();
+        if stat.kind == io::TypeFile && (stat.perm & io::UserExecute != 0) {
+            let name = i.filename_str().unwrap();
+            if name.starts_with("test_") {
+                let mut run = Process::new(i.as_str().unwrap(), &[], ProcessOptions::new()).unwrap();
+                let ret = run.finish();
+                if !ret.success() {
+                    println!("Unit test '{}' failed.", name);
+                    os::set_exit_status(1);
+                    return;
+                }
+            }
+        }
+    }
+}
