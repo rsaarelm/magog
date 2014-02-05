8e4d3959229e3aad4f080a4422f9bc368349e5ca
Risto Saarelma <risto.saarelma@iki.fi>
2014-02-05 21:09:46 +0200

    Texture recter in app

--- src/glutil/app.rs
+++ src/glutil/app.rs
@@ -1,10 +1,15 @@
+use std::mem;
 use opengles::gl2;
 use cgmath::vector::{Vec2, Vec4};
+use cgmath::point::{Point2, Point3};
+use cgmath::aabb::{Aabb, Aabb2};
 use glfw;
 use texture::Texture;
 use shader::Shader;
 use fonter::Fonter;
 use stb;
+
+use gl_check;
 
 static VERTEX_SHADER: &'static str =
     "#version 130
@@ -39,6 +44,92 @@ static FONT_NUM_CHARS: uint = 95;
 // TODO: Make a proper type.
 type Color = Vec4<u8>;
 
+struct Recter {
+    vertices: ~[Point3<f32>],
+    texcoords: ~[Point2<f32>],
+    colors: ~[Color],
+}
+
+impl Recter {
+    fn new() -> Recter {
+        Recter {
+            vertices: ~[],
+            texcoords: ~[],
+            colors: ~[],
+        }
+    }
+
+    fn add(&mut self, area: &Aabb2<f32>, texcoords: &Aabb2<f32>, color: &Color) {
+        self.vertices.push(Point3::new(area.min().x, area.min().y, 0.0));
+        self.texcoords.push(Point2::new(texcoords.min().x, texcoords.max().y));
+        self.colors.push(*color);
+
+        self.vertices.push(Point3::new(area.min().x, area.max().y, 0.0));
+        self.texcoords.push(Point2::new(texcoords.min().x, texcoords.min().y));
+        self.colors.push(*color);
+
+        self.vertices.push(Point3::new(area.max().x, area.max().y, 0.0));
+        self.texcoords.push(Point2::new(texcoords.max().x, texcoords.min().y));
+        self.colors.push(*color);
+
+        self.vertices.push(Point3::new(area.min().x, area.min().y, 0.0));
+        self.texcoords.push(Point2::new(texcoords.min().x, texcoords.max().y));
+        self.colors.push(*color);
+
+        self.vertices.push(Point3::new(area.max().x, area.max().y, 0.0));
+        self.texcoords.push(Point2::new(texcoords.max().x, texcoords.min().y));
+        self.colors.push(*color);
+
+        self.vertices.push(Point3::new(area.max().x, area.min().y, 0.0));
+        self.texcoords.push(Point2::new(texcoords.max().x, texcoords.max().y));
+        self.colors.push(*color);
+    }
+
+    fn clear(&mut self) {
+        self.vertices = ~[];
+        self.texcoords = ~[];
+        self.colors = ~[];
+    }
+
+    fn render(&mut self, shader: &Shader) {
+        // Generate buffers.
+        // TODO: Wrap STREAM_DRAW buffers into RAII handles.
+	let gen = gl_check!(gl2::gen_buffers(3));
+	let vert = gen[0];
+	let tex = gen[1];
+	let col = gen[2];
+
+	gl_check!(gl2::bind_buffer(gl2::ARRAY_BUFFER, vert));
+	gl_check!(gl2::buffer_data(gl2::ARRAY_BUFFER, self.vertices, gl2::STREAM_DRAW));
+
+	gl_check!(gl2::bind_buffer(gl2::ARRAY_BUFFER, tex));
+	gl_check!(gl2::buffer_data(gl2::ARRAY_BUFFER, self.texcoords, gl2::STREAM_DRAW));
+
+	gl_check!(gl2::bind_buffer(gl2::ARRAY_BUFFER, col));
+	gl_check!(gl2::buffer_data(gl2::ARRAY_BUFFER, self.colors, gl2::STREAM_DRAW));
+
+        // Bind shader vars.
+        let in_pos = shader.attrib("in_pos").unwrap();
+	gl_check!(gl2::enable_vertex_attrib_array(in_pos));
+	gl_check!(gl2::bind_buffer(gl2::ARRAY_BUFFER, vert));
+	gl_check!(gl2::vertex_attrib_pointer_f32(in_pos, 3, false, mem::size_of::<Point3<f32>>() as i32, 0));
+        let in_texcoord = shader.attrib("in_texcoord").unwrap();
+	gl_check!(gl2::enable_vertex_attrib_array(in_texcoord));
+	gl_check!(gl2::bind_buffer(gl2::ARRAY_BUFFER, tex));
+	gl_check!(gl2::vertex_attrib_pointer_f32(in_texcoord, 2, false, mem::size_of::<Point2<f32>>() as i32, 0));
+        // TODO: Color in shader
+
+	gl_check!(gl2::draw_arrays(gl2::TRIANGLES, 0, self.vertices.len() as i32));
+	gl_check!(gl2::bind_buffer(gl2::ARRAY_BUFFER, 0));
+
+	gl_check!(gl2::disable_vertex_attrib_array(in_texcoord));
+	gl_check!(gl2::disable_vertex_attrib_array(in_pos));
+
+        self.clear();
+        gl2::delete_buffers(gen);
+    }
+}
+
 pub struct App {
     resolution: Vec2<f32>,
     draw_color: Color,
@@ -47,6 +138,7 @@ pub struct App {
     fonter: ~Fonter,
     texture: Option<~Texture>,
     shader: ~Shader,
+    recter: Recter,
 }
 
 impl App {
@@ -75,6 +167,7 @@ impl App {
             fonter: ~Fonter::new(&truetype, FONT_SIZE, FONT_START_CHAR, FONT_NUM_CHARS),
             texture: None,
             shader: ~Shader::new(VERTEX_SHADER, FRAGMENT_SHADER),
+            recter: Recter::new(),
         };
 
         ret.shader.bind();
@@ -87,7 +180,13 @@ impl App {
     }
 
     pub fn draw_string(&mut self, _pos: Vec2<f32>, _text: &str) {
-        self.fonter.test(self.shader);
+        //self.fonter.test(self.shader);
+        self.recter.add(
+            &Aabb2::new(&Point2::new(0.0f32, 0.0f32), &Point2::new(1.0f32, 1.0f32)),
+            &Aabb2::new(&Point2::new(0.0f32, 0.0f32), &Point2::new(1.0f32, 1.0f32)),
+            &Vec4::new(255u8, 0u8, 0u8, 255u8));
+        self.fonter.bind();
+        self.recter.render(self.shader);
         // TODO: This is where we'd add rectangles of the characters to the
         // rect renderer.
     }
