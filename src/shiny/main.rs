6f73c534e78c0d4d5b852883aa0a29b963da0120
Risto Saarelma <risto.saarelma@iki.fi>
2014-02-08 10:57:28 +0200

    Test pattern for pixel perfection

--- src/shiny/main.rs
+++ src/shiny/main.rs
@@ -3,20 +3,28 @@ extern mod opengles;
 extern mod cgmath;
 extern mod stb;
 extern mod glutil;
+extern mod calx;
 
 use glutil::app::App;
 use cgmath::aabb::{Aabb2};
-use cgmath::point::{Point2};
+use cgmath::point::{Point, Point2};
 use cgmath::vector::{Vec2, Vec4};
+use calx::rectutil::RectUtil;
 
 pub fn main() {
     let mut app = App::new(640, 360, "Shiny!");
     while app.alive {
         glfw::poll_events();
+        app.set_color(&Vec4::new(0.0f32, 0.1f32, 0.2f32, 1f32));
+        app.fill_rect(&Aabb2::new(
+                &Point2::new(0.0f32, 0.0f32),
+                &Point2::new(640.0f32, 360.0f32)));
         app.set_color(&Vec4::new(0.1f32, 0.3f32, 0.6f32, 1f32));
-        app.fill_rect(
-            &Aabb2::new(&Point2::new(0.0f32, 0.0f32),
-            &Point2::new(640.0f32, 360.0f32)));
+        for p in Aabb2::new(&Point2::new(0f32, 0f32), &Point2::new(213f32, 120f32)).points() {
+            app.fill_rect(&Aabb2::new(
+                    &p.mul_s(3f32),
+                    &p.mul_s(3f32).add_v(&Vec2::new(2f32, 2f32))));
+        }
         app.set_color(&Vec4::new(0.5f32, 1f32, 0.5f32, 1f32));
         app.draw_string(&Vec2::new(0f32, 8f32), "The quick brown fox jumps over the lazy dog.");
         app.draw_string(&Vec2::new(0f32, 18f32), "Grumpy wizards make toxic brew for the evil Queen and Jack.");
