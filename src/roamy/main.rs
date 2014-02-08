b71ddf92b6c2062eeac3a881f7f5ce82ae89df1a
Risto Saarelma <risto.saarelma@iki.fi>
2014-02-09 00:12:55 +0200

    Isometric map tile display demo

--- src/roamy/main.rs
+++ src/roamy/main.rs
@@ -1,3 +1,118 @@
+extern mod cgmath;
+extern mod glutil;
+extern mod calx;
+extern mod stb;
+
+use std::hashmap::HashMap;
+
+use glutil::app::App;
+use glutil::atlas::Sprite;
+use cgmath::aabb::{Aabb2};
+use cgmath::point::{Point, Point2};
+use cgmath::vector::{Vec2, Vec4};
+use calx::rectutil::RectUtil;
+use stb::image::Image;
+
+use std::rand::random;
+
+#[deriving(Eq)]
+enum TerrainType {
+    Wall,
+    Floor,
+}
+
+struct Area {
+    set: HashMap<Point2<i8>, TerrainType>,
+}
+
+impl Area {
+    pub fn new() -> Area {
+        let mut ret = Area {
+            set: HashMap::new(),
+        };
+        for _i in range(0, 256) {
+            let x : i8 = random();
+            let y : i8 = random();
+            ret.set.insert(Point2::new(x % 16, y % 16), Floor);
+        }
+        ret
+    }
+
+    pub fn get(&self, p: &Point2<i8>) -> TerrainType {
+        match self.set.find(p) {
+            None => Wall,
+            Some(&t) => t
+        }
+    }
+}
+
 pub fn main() {
-    println!("Roamy: A program for testing map generation.");
+    let mut app = App::new(640, 360, "Mapgen demo");
+    let tiles = Image::load("assets/tile.png", 1).unwrap();
+    let sprites = Sprite::new_alpha_set(
+        &Vec2::new(32, 32),
+        &Vec2::new(tiles.width as int, tiles.height as int),
+        tiles.pixels,
+        &Vec2::new(-16, -16));
+    let idx = app.add_sprite(~sprites[0].clone());
+    app.add_sprite(~sprites[1].clone());
+    app.add_sprite(~sprites[2].clone());
+    app.add_sprite(~sprites[3].clone());
+    app.add_sprite(~sprites[4].clone());
+    app.add_sprite(~sprites[5].clone());
+    let area = Area::new();
+    // TODO: Draw a map view on screen with tiles.
+    while app.alive {
+        app.set_color(&Vec4::new(0.0f32, 0.1f32, 0.2f32, 1f32));
+        app.fill_rect(&RectUtil::new(0.0f32, 0.0f32, 640.0f32, 360.0f32));
+        app.set_color(&Vec4::new(0.1f32, 0.3f32, 0.6f32, 1f32));
+        /*
+        let area : Aabb2<f32> = RectUtil::new(0.0f32, 0.0f32, 213.0f32, 120.0f32);
+        for p in area.points() {
+            app.fill_rect(&Aabb2::new(
+                    &p.mul_s(2f32),
+                    &p.mul_s(2f32).add_v(&Vec2::new(1f32, 1f32))));
+        }
+        */
+        let rect : Aabb2<i8> = RectUtil::new(0i8, 0i8, 16i8, 16i8);
+        for p in rect.points() {
+            let offset = Vec2::new(
+                320.0 + 16.0 * (p.x as f32) - 16.0 * (p.y as f32),
+                24.0 + 8.0 * (p.x as f32) + 8.0 * (p.y as f32));
+            app.set_color(&Vec4::new(0.7f32, 0.7f32, 0.8f32, 1f32));
+            // Floor
+            app.draw_sprite(idx, &offset);
+            if area.get(&p) == Wall {
+                // XXX: Horrible prototype code, figure out cleaning.
+                app.set_color(&Vec4::new(0.6f32, 0.5f32, 0.1f32, 1f32));
+                let left = area.get(&p.add_v(&Vec2::new(-1i8, 0i8))) == Wall;
+                let rear = area.get(&p.add_v(&Vec2::new(-1i8, -1i8))) == Wall;
+                let right = area.get(&p.add_v(&Vec2::new(0i8, -1i8))) == Wall;
+                if left && right && rear {
+                    app.draw_sprite(idx + 1, &offset);
+                    if area.get(&p.add_v(&Vec2::new(1i8, -1i8))) != Wall ||
+                       area.get(&p.add_v(&Vec2::new(1i8, 0i8))) != Wall {
+                        app.draw_sprite(idx + 3, &offset);
+                    }
+                    if area.get(&p.add_v(&Vec2::new(-1i8, 1i8))) != Wall ||
+                       area.get(&p.add_v(&Vec2::new(0i8, 1i8))) != Wall {
+                        app.draw_sprite(idx + 2, &offset);
+                    }
+                    if area.get(&p.add_v(&Vec2::new(1i8, 1i8))) != Wall {
+                        app.draw_sprite(idx + 5, &offset);
+                    }
+                } else if left && right {
+                    app.draw_sprite(idx + 4, &offset);
+                } else if left {
+                    app.draw_sprite(idx + 2, &offset);
+                } else if right {
+                    app.draw_sprite(idx + 3, &offset);
+                } else {
+                    app.draw_sprite(idx + 5, &offset);
+                };
+            }
+        }
+
+        app.flush();
+    }
 }
