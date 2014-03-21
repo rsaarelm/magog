ea6ceada86ddcd6a226d2ef5714e04833686e0d9
Risto Saarelma <risto.saarelma@iki.fi>
2014-03-21 21:01:49 +0200

    Stub crates for game world logic and display

---
+++ src/view/lib.rs
@@ -0,0 +1,4 @@
+#[crate_id = "view"];
+#[desc = "Game world display"];
+#[feature(globs)];
+#[crate_type = "rlib"];
---
+++ src/world/lib.rs
@@ -0,0 +1,3 @@
+#[crate_id = "world"];
+#[desc = "Game world logic"];
+#[crate_type = "rlib"];
