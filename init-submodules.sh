c9a232f225d4ce3b6fc178aaf4060383b2342df5
Risto Saarelma <risto.saarelma@iki.fi>
2014-04-18 18:14:51 +0300

    Helper script for hard-to-tup build bits

---
+++ init-submodules.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+git submodule update --init
+
+# Build native GLFW.
+mkdir -p .build/glfw
+cd .build/glfw
+cmake -DCMAKE_C_FLAGS="-fPIC -Os -DNDEBUG" ../../lib/glfw
+make
+cp src/libglfw3.a ../../libglfw.a
+cd -
+
+# Build native Portaudio
+mkdir -p .build/
+cp -r lib/portaudio .build/
+cd .build/portaudio
+./configure CFLAGS="-fPIC -Os -DNDEBUG"
+make
+cp lib/.libs/libportaudio.a ../../
+cd -
+
+# Remove the temporary build directory to keep tup from complaining.
+rm -rf .build/
+
+cd lib/gl-rs
+make lib
+rm -rf examples/ lib/ bin/
+rm deps/sax-rs/lib/*.rlib
+rm deps/glfw-rs/src/lib/link.rs
+cd -
+
+cd lib/glfw-rs
+make lib
+rm -rf lib/
+cd -
