8eebf2d060d342e8c1653e6ac72cba9342a8c9b7
Risto Saarelma <risto.saarelma@iki.fi>
2014-06-26 12:25:12 +0300

    Use cargo to build project

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script for clean before a rebuild. Since tup doesn't do this for us.
-
-# Exclude assets dir from clean, might be keeping some scratch art there.
-git clean -fdx -e assets/
-# Some submodules may have generated files that mess up Tup build, need to
-# clean up those separately.
-git submodule foreach git clean -fdx
--- rust-rule.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SRC=$1
-TARG=$2
-shift 2; TAIL=$*
-echo ": $SRC $TAIL |> !rustlib |> `rustc --crate-type rlib --crate-file-name $SRC | tr '\\n' ' '` $TARG"
