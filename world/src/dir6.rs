04efe07e8472eeb9d0af5153bddcc4099e7afde3
Risto Saarelma <risto.saarelma@iki.fi>
2014-10-19 10:51:22 +0300

    Dir6 commenting and unit tests

--- world/src/dir6.rs
+++ world/src/dir6.rs
@@ -25,6 +25,25 @@ impl Dir6 {
             (radian / width).floor() as int
         };
 
+        // The hexadecants (00 to 15) and the hex directions (*0* to *5*) around
+        // the origin.
+        //
+        //    *0*       *1*
+        //       \ 14 15 | 00 01
+        //       13\     |      02
+        //           \   |
+        //     12      \ |        03
+        // *5* ----------O-X------- *2*
+        //     11        Y \      04
+        //               |   \
+        //       10      |     \05
+        //         09 08 | 07 06 \
+        //              *4*       *3*
+        //
+        // Vectors that are in a space between two hex direction vectors are
+        // snapped to the hex direction vector nearer to them. The hexadecants
+        // are a convenience structure that lets us assign every vector using
+        // the single hexadecant value.
         Dir6::from_int(match hexadecant {
             13 | 14 => 0,
             15 | 0 | 1 => 1,
@@ -78,23 +97,30 @@ mod test {
         assert_eq!(NorthWest, Dir6::from_int(5));
         assert_eq!(NorthEast, Dir6::from_int(1));
 
+        assert_eq!(NorthEast, Dir6::from_v2(V2(20, -21)));
+        assert_eq!(SouthEast, Dir6::from_v2(V2(20, -10)));
+        assert_eq!(North, Dir6::from_v2(V2(-10, -10)));
+        assert_eq!(South, Dir6::from_v2(V2(1, 1)));
+
         for i in range(0, 6) {
             let d = Dir6::from_int(i);
             let v = d.to_v2();
             let v1 = Dir6::from_int(i - 1).to_v2();
             let v2 = Dir6::from_int(i + 1).to_v2();
+
+            // Test static iter
+            assert_eq!(Some(d), Dir6::iter().nth(i as uint).map(|&x| x));
+
+            // Test vector mapping.
             assert_eq!(d, Dir6::from_v2(v));
+
+            // Test opposite dir vector mapping.
             assert_eq!(Dir6::from_int(i + 3), Dir6::from_v2(-v));
-            assert_eq!(d, Dir6::from_v2(v * 6));
-            // Test deviated longer vecs.
+
+            // Test approximation of longer vectors.
+            assert_eq!(d, Dir6::from_v2(v * 3));
             assert_eq!(d, Dir6::from_v2(v * 3 + v1));
             assert_eq!(d, Dir6::from_v2(v * 3 + v2));
         }
-        /*
-        assert_eq!(NorthEast, Dir6::from_v2(V2(20, -21)));
-        assert_eq!(SouthEast, Dir6::from_v2(V2(20, -10)));
-        assert_eq!(North, Dir6::from_v2(V2(-10, -10)));
-        assert_eq!(South, Dir6::from_v2(V2(1, 1)));
-        */
     }
 }
