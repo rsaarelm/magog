7a5678d0ea103914a3e9d2fd3500949ed91a1503
Risto Saarelma <risto.saarelma@iki.fi>
2015-01-12 18:59:13 +0200

    Comment explaining the sprite fragment shader discard

--- backend/src/renderer.rs
+++ backend/src/renderer.rs
@@ -44,7 +44,11 @@ impl Renderer {
 
                 void main() {
                     vec4 tex_color = texture2D(texture, v_tex_coord);
+
+                    // Discard fully transparent pixels to keep them from
+                    // writing into the depth buffer.
                     if (tex_color.a == 0.0) discard;
+
                     gl_FragColor = v_color * tex_color;
                 }
             ",
