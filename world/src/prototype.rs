f1015a72da4fdfb4a073f0ca418df957d4605dea
Risto Saarelma <risto.saarelma@iki.fi>
2015-01-22 11:32:47 +0200

    More compact prototype def

--- world/src/prototype.rs
+++ world/src/prototype.rs
@@ -1,5 +1,5 @@
 use std::default::Default;
-use util::color;
+use util::color::*;
 use ecs::{Component};
 use entity::{Entity};
 use components::{Spawn, Category};
@@ -45,69 +45,69 @@ pub fn init() {
     // Player
     Prototype::new(Some(base_mob))
         (Brain { state: BrainState::PlayerControl, alignment: Alignment::Good })
-        (Desc::new("player", 51, color::AZURE))
+        (Desc::new("player", 51, AZURE))
         (Stats::new(6, &[Hands]))
         (MapMemory::new())
         ;
 
     // Enemies
     Prototype::new(Some(base_mob))
-        (Desc::new("dreg", 72, color::OLIVE))
+        (Desc::new("dreg", 72, OLIVE))
         (Stats::new(1, &[Hands]))
         (Spawn::new(Category::Mob))
         ;
 
     Prototype::new(Some(base_mob))
-        (Desc::new("snake", 71, color::GREEN))
+        (Desc::new("snake", 71, GREEN))
         (Stats::new(1, &[]))
         (Spawn::new(Category::Mob).biome(Overland))
         ;
 
     Prototype::new(Some(base_mob))
-        (Desc::new("ooze", 77, color::LIGHTSEAGREEN))
+        (Desc::new("ooze", 77, LIGHTSEAGREEN))
         (Stats::new(3, &[]))
         (Spawn::new(Category::Mob).biome(Dungeon).depth(3))
         ;
 
     Prototype::new(Some(base_mob))
-        (Desc::new("ogre", 73, color::DARKSLATEGRAY))
+        (Desc::new("ogre", 73, DARKSLATEGRAY))
         (Stats::new(6, &[Hands]))
         (Spawn::new(Category::Mob).depth(5))
         ;
 
     Prototype::new(Some(base_mob))
-        (Desc::new("wraith", 74, color::HOTPINK))
+        (Desc::new("wraith", 74, HOTPINK))
         (Stats::new(8, &[Hands]))
         (Spawn::new(Category::Mob).biome(Dungeon).depth(6))
         ;
 
     Prototype::new(Some(base_mob))
-        (Desc::new("octopus", 63, color::DARKTURQUOISE))
+        (Desc::new("octopus", 63, DARKTURQUOISE))
         (Stats::new(10, &[]))
         (Spawn::new(Category::Mob).depth(7))
         ;
 
     Prototype::new(Some(base_mob))
-        (Desc::new("efreet", 78, color::ORANGE))
+        (Desc::new("efreet", 78, ORANGE))
         (Stats::new(12, &[]))
         (Spawn::new(Category::Mob).depth(8))
         ;
 
     Prototype::new(Some(base_mob))
-        (Desc::new("serpent", 94, color::CORAL))
+        (Desc::new("serpent", 94, CORAL))
         (Stats::new(15, &[]))
         (Spawn::new(Category::Mob).biome(Dungeon).depth(9))
         ;
 
     // Items
     Prototype::new(None)
-        (Desc::new("heart", 89, color::RED))
+        (Desc::new("heart", 89, RED))
         (Spawn::new(Category::Consumable))
         (Item { item_type: ItemType::Instant, ability: Ability::HealInstant(2) })
         ;
 
     Prototype::new(None)
-        (Desc::new("sword", 84, color::GAINSBORO))
+        (Desc::new("sword", 84, GAINSBORO))
         (Spawn::new(Category::Equipment).commonness(100))
         (Stats::new(5, &[]))
         (Item { item_type: ItemType::MeleeWeapon, ability: Ability::Multi(vec![]) })
