f61d71062332446cdd0f166984561484e0a6b2b5
Risto Saarelma <risto.saarelma@iki.fi>
2015-02-25 10:01:52 +0200

    Read both git and rustc versions

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
-#![feature(io, path)]
+#![feature(old_io, old_path)]
 
 // Generate the git version string.
 
 use std::old_io::process::{Command, ProcessOutput};
 use std::old_io::{File};
 
-fn get_version() -> String {
+fn git_version() -> String {
     match Command::new("git")
         .arg("log").arg("--pretty=format:%h").arg("-1").output() {
         Ok(ProcessOutput { status: exit, output: out, error: err }) => {
@@ -22,10 +22,31 @@ fn get_version() -> String {
     return "unknown".to_string();
 }
 
+fn rustc_version() -> String {
+    match Command::new("rustc")
+        .arg("--version").output() {
+        Ok(ProcessOutput { status: exit, output: out, error: err }) => {
+            if exit.success() {
+                return String::from_utf8(out).unwrap();
+            } else {
+                println!("Error getting rustc version: {}", String::from_utf8(err).unwrap());
+            }
+        }
+        Err(err) => {
+            println!("Error getting rustc version: {}", err);
+        }
+    }
+    return "unknown".to_string();
+}
+
+fn open(path: &str) -> File {
+    match File::create(&Path::new(path)) {
+        Ok(f) => f, Err(e) => panic!("file error: {}", e),
+    }
+}
+
 pub fn main() {
     // Write the current Git HEAD hash into the version file.
-    let mut file = match File::create(&Path::new("src/version.inc")) {
-        Ok(f) => f, Err(e) => panic!("file error: {}", e),
-    };
-    write!(&mut file, "{}", get_version()).unwrap();
+    write!(&mut open("src/git_version.inc"), "{}", git_version()).unwrap();
+    write!(&mut open("src/rustc_version.inc"), "{}", rustc_version()).unwrap();
 }
