c3e8ea2134cee974e01cb21fcae606ef3efdcf9f
Risto Saarelma <risto.saarelma@iki.fi>
2015-03-19 12:57:31 +0200

    Multithreaded screenshotting

--- src/main.rs
+++ src/main.rs
@@ -58,30 +58,49 @@ pub fn compiler_version() -> String {
 pub fn screenshot(ctx: &mut Canvas) {
     use time;
     use std::path;
-    use std::fs::PathExt;
+    use std::fs::{self, File, PathExt};
+    use std::thread;
     use image;
 
-    let timestamp = time::precise_time_s() as u64;
-
-    // Create screenshot filenames by concatenating the current timestamp in
-    // seconds with a running number from 00 to 99. 100 shots per second
-    // should be good enough.
-
-    // Default if we fail to generate any of the 100 candidates for this
-    // second, just overwrite with the "xx" prefix then.
-    let mut filename = format!("magog-{}{}.png", timestamp, "xx");
-
-    // Run through candidates for this second.
-    for i in 0..100 {
-        let test_filename = format!("magog-{}{:02}.png", timestamp, i);
-        if !path::Path::new(&test_filename).exists() {
-            filename = test_filename;
-            break;
-        }
-    }
-
     let shot = ctx.screenshot();
-    let _ = image::save_buffer(&Path::new(filename), shot.as_slice(), shot.width(), shot.height(), image::ColorType::RGB(8));
+
+    // XXX: If the game is terminated right after taking a screenshot, the
+    // screenshotting thread will be stopped and an incomplete image file will
+    // result. Not sure if there's a better solution for this than some kind
+    // of global worker thread pool where the screenshot thread should be
+    // added.
+    thread::spawn(move || {
+        let timestamp = time::precise_time_s() as u64;
+        // Create screenshot filenames by concatenating the current timestamp in
+        // seconds with a running number from 00 to 99. 100 shots per second
+        // should be good enough.
+
+        // Default if we fail to generate any of the 100 candidates for this
+        // second, just overwrite with the "xx" prefix then.
+        let mut filename = format!("magog-{}{}.png", timestamp, "xx");
+
+        // Run through candidates for this second.
+        for i in 0..100 {
+            let test_filename = format!("magog-{}{:02}.png", timestamp, i);
+            if !path::Path::new(&test_filename).exists() {
+                // Thread-safe claiming: create_dir will fail if the dir
+                // already exists (it'll exist if another thread is gunning
+                // for the same filename and managed to get past us here).
+                // At least assuming that create_dir is atomic...
+                let squat_dir = format!(".tmp-{}{:02}", timestamp, i);
+                if std::fs::create_dir(&squat_dir).is_ok() {
+                    File::create(&test_filename).unwrap();
+                    filename = test_filename;
+                    fs::remove_dir(&squat_dir).unwrap();
+                    break;
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        let _ = image::save_buffer(&Path::new(filename), shot.as_slice(), shot.width(), shot.height(), image::ColorType::RGB(8));
+    });
 }
 
 pub fn main() {
