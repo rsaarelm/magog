89ed58353c40802359118086e707fdd81ec45b23
Risto Saarelma <risto.saarelma@iki.fi>
2015-04-17 16:23:52 +0300

    Don't multithread saving screenshots
    Multithreading without a worker pool architecture causes a bug where
    quitting the game right after starting screenshot capture aborts the
    spawned the thread mid-write and leaves malformed images behind.
    
    Instead of setting up some sort of fancy architecture that keeps
    track of the worker threads and only exits the program after all
    important tasks are complete, I'm just going to be lazy and push the
    screenshot encoding into the main thread.

--- src/main.rs
+++ src/main.rs
@@ -81,48 +81,40 @@ pub fn screenshot(ctx: &mut Canvas) {
     use time;
     use std::path::{Path};
     use std::fs::{self, File, PathExt};
-    use std::thread;
     use image;
 
     let shot = ctx.screenshot();
 
-    // XXX: If the game is terminated right after taking a screenshot, the
-    // screenshotting thread will be stopped and an incomplete image file will
-    // result. Not sure if there's a better solution for this than some kind
-    // of global worker thread pool where the screenshot thread should be
-    // added.
-    thread::spawn(move || {
-        let timestamp = time::precise_time_s() as u64;
-        // Create screenshot filenames by concatenating the current timestamp in
-        // seconds with a running number from 00 to 99. 100 shots per second
-        // should be good enough.
+    let timestamp = time::precise_time_s() as u64;
+    // Create screenshot filenames by concatenating the current timestamp in
+    // seconds with a running number from 00 to 99. 100 shots per second
+    // should be good enough.
 
-        // Default if we fail to generate any of the 100 candidates for this
-        // second, just overwrite with the "xx" prefix then.
-        let mut filename = format!("magog-{}{}.png", timestamp, "xx");
+    // Default if we fail to generate any of the 100 candidates for this
+    // second, just overwrite with the "xx" prefix then.
+    let mut filename = format!("magog-{}{}.png", timestamp, "xx");
 
-        // Run through candidates for this second.
-        for i in 0..100 {
-            let test_filename = format!("magog-{}{:02}.png", timestamp, i);
-            if !Path::new(&test_filename).exists() {
-                // Thread-safe claiming: create_dir will fail if the dir
-                // already exists (it'll exist if another thread is gunning
-                // for the same filename and managed to get past us here).
-                // At least assuming that create_dir is atomic...
-                let squat_dir = format!(".tmp-{}{:02}", timestamp, i);
-                if std::fs::create_dir(&squat_dir).is_ok() {
-                    File::create(&test_filename).unwrap();
-                    filename = test_filename;
-                    fs::remove_dir(&squat_dir).unwrap();
-                    break;
-                } else {
-                    continue;
-                }
+    // Run through candidates for this second.
+    for i in 0..100 {
+        let test_filename = format!("magog-{}{:02}.png", timestamp, i);
+        if !Path::new(&test_filename).exists() {
+            // Thread-safe claiming: create_dir will fail if the dir
+            // already exists (it'll exist if another thread is gunning
+            // for the same filename and managed to get past us here).
+            // At least assuming that create_dir is atomic...
+            let squat_dir = format!(".tmp-{}{:02}", timestamp, i);
+            if std::fs::create_dir(&squat_dir).is_ok() {
+                File::create(&test_filename).unwrap();
+                filename = test_filename;
+                fs::remove_dir(&squat_dir).unwrap();
+                break;
+            } else {
+                continue;
             }
         }
+    }
 
-        let _ = image::save_buffer(&Path::new(&filename), &shot, shot.width(), shot.height(), image::ColorType::RGB(8));
-    });
+    let _ = image::save_buffer(&Path::new(&filename), &shot, shot.width(), shot.height(), image::ColorType::RGB(8));
 }
 
 pub fn main() {
