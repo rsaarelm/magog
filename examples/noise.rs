eec6a33ad15c57841106f685245923f180604691
Risto Saarelma <risto.saarelma@iki.fi>
2015-07-12 11:55:22 +0300

    Noise is a bit more quiet

--- examples/noise.rs
+++ examples/noise.rs
@@ -5,8 +5,8 @@ use calx::backend::{Mixer};
 fn main() {
     println!("Starting noise");
     let mut mixer = Mixer::new();
-    mixer.add_wave(Box::new(|t| (t * 3000.0).sin()), 2.0);
+    mixer.add_wave(Box::new(|t| (t * 3000.0).sin() * 0.2), 2.0);
     thread::sleep_ms(1000);
-    mixer.add_wave(Box::new(|t| (t * 1000.0).sin()), 2.0);
+    mixer.add_wave(Box::new(|t| (t * 1000.0).sin() * 0.2), 2.0);
     thread::sleep_ms(3000);
 }
