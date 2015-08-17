7f5e48a77ff59b888055b81183d9a6ee7fbbbaac
Risto Saarelma <risto.saarelma@iki.fi>
2015-08-17 11:02:13 +0300

    Merge macros crate to main calx

--- calx_macros/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#[macro_export]
-macro_rules! count_exprs {
-    () => { 0 };
-    ($e:expr) => { 1 };
-    ($e:expr, $($es:expr),+) => { 1 + count_exprs!($($es),*) };
-}
--- src/lib.rs
+++ src/lib.rs
@@ -178,6 +178,13 @@ impl AverageDuration {
     }
 }
 
+#[macro_export]
+macro_rules! count_exprs {
+    () => { 0 };
+    ($e:expr) => { 1 };
+    ($e:expr, $($es:expr),+) => { 1 + count_exprs!($($es),*) };
+}
+
 
 #[cfg(test)]
 mod test {
