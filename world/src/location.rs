bb6baffccd435d95ee8581fb9d09efeed239a24b
Risto Saarelma <risto.saarelma@iki.fi>
2015-10-03 11:41:13 +0300

    Cleanup defunct code

--- world/src/location.rs
+++ world/src/location.rs
@@ -13,73 +13,6 @@ pub struct Location {
 impl Location {
     pub fn new(x: i8, y: i8) -> Location { Location { x: x, y: y } }
 
-    /*
-    /// Return terrain at the location.
-    pub fn terrain(&self) -> TerrainType {
-        let mut ret = world::with(|w| w.area.terrain(*self));
-        // Mobs standing on doors make the doors open.
-        if ret == TerrainType::Door && self.has_mobs() {
-            ret = TerrainType::OpenDoor;
-        }
-        // Grass is only occasionally fancy.
-        // TODO: Make variant tiles into a generic method.
-        if ret == TerrainType::Grass {
-            let n = noise(self.x as i32 + self.y as i32 * 57);
-            if n > 0.85 {
-                ret = TerrainType::Grass2;
-            }
-        }
-        ret
-    }
-
-    pub fn blocks_sight(&self) -> bool {
-        self.terrain().blocks_sight()
-    }
-
-    pub fn blocks_walk(&self) -> bool {
-        if self.terrain().blocks_walk() { return true; }
-        if self.entities().iter().any(|e| e.blocks_walk()) {
-            return true;
-        }
-        false
-    }
-
-    pub fn entities(&self) -> Vec<Entity> {
-        world::with(|w| w.spatial.entities_at(*self))
-    }
-
-    /// Return the most significant entity at the location.
-    pub fn main_entity(&self) -> Option<Entity> {
-        let e = self.entities();
-        // TODO: Actually make the result entity the most significant one (eg.
-        // mob if there are both mobs and items).
-        if e.len() > 0 { Some(e[0]) } else { None }
-    }
-
-    /// Return an item at the location that can be interacted with.
-    pub fn top_item(&self) -> Option<Entity> {
-        self.entities().iter().find(|e| e.can_be_picked_up()).map(|&x| x)
-    }
-
-    pub fn _has_entities(&self) -> bool { !self.entities().is_empty() }
-
-    pub fn has_mobs(&self) -> bool {
-        self.entities().iter().any(|e| e.is_mob())
-    }
-
-    /// Returns the mob at the given location, if any. The assumption is that
-    /// there is always a single primary mob in locations that contain
-    /// multiple mobs that can be selected as the return value.
-    pub fn mob_at(&self) -> Option<Entity> {
-        for &e in self.entities().iter() {
-            if e.is_mob() {
-                return Some(e);
-            }
-        }
-        None
-    }
-    */
-
     /// Vector pointing from this location into the other one if the locations
     /// are on the same Euclidean plane.
     pub fn v2_at(&self, other: Location) -> Option<V2<i32>> {
@@ -100,59 +33,6 @@ impl Location {
     pub fn noise(&self) -> f32 {
         noise(self.x as i32 + self.y as i32 * 57)
     }
-
-    /*
-    /// Return the status of the location in the player's field of view.
-    /// Returns None if the location is unexplored.
-    pub fn fov_status(&self) -> Option<::FovStatus> {
-        if let Some(p) = action::player() {
-            match world::with(|w| {
-                if let Some(ref mm) = w.map_memories().get(p) {
-                    Ok (if mm.seen.contains(self) {
-                        Some(::FovStatus::Seen)
-                    } else if mm.remembered.contains(self) {
-                        Some(::FovStatus::Remembered)
-                    } else {
-                        None
-                    })
-                } else {
-                    Err(())
-                }
-            }) {
-                Ok(x) => return x,
-                _ => ()
-            };
-        }
-        // Just show everything by default.
-        Some(::FovStatus::Seen)
-    }
-
-    /// Area name for the location.
-    pub fn name(&self) -> String {
-        match action::current_depth() {
-            0 => "Limbo".to_string(),
-            1 => "Overworld".to_string(),
-            n => format!("Dungeon {}", n - 1)
-        }
-    }
-
-    /// Light level for the location.
-    pub fn light(&self) -> Light {
-        if action::current_depth() == 1 {
-            // Topside, full light.
-            return Light::new(1.0);
-        }
-        if self.terrain().is_luminous() {
-            return Light::new(1.0);
-        }
-
-        if let Some(d) = self.distance_from(flags::camera()) {
-            let lum = 0.8 - d as f32 / 10.0;
-            return Light::new(if lum >= 0.0 { lum } else { 0.0 });
-        }
-        return Light::new(1.0);
-    }
-    */
 }
 
 impl Add<V2<i32>> for Location {
