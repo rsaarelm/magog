517de1293ae7ef5a30ed7c916ad13442dc8278b3
Risto Saarelma <risto.saarelma@iki.fi>
2016-01-17 11:45:34 +0200

    ECS bincode is dev-dependency only

--- calx_ecs/src/lib.rs
+++ calx_ecs/src/lib.rs
@@ -6,7 +6,6 @@
 #![plugin(serde_macros)]
 
 extern crate serde;
-extern crate bincode;
 
 use std::default::Default;
 use std::ops::{Deref, DerefMut, Index, IndexMut};
@@ -278,68 +277,5 @@ macro_rules! Ecs {
 macro_rules! build_mask {
     ( $($compname:ident),+ ) => {
         0u64 $(| (1u64 << ComponentNum::$compname as u8))+
-    }
-}
-
-#[cfg(test)]
-mod test {
-    #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
-    pub struct Desc {
-        name: String,
-        icon: usize,
-    }
-
-    #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
-    pub struct Pos {
-        x: i32,
-        y: i32,
-    }
-
-    Ecs! {
-        desc: Desc,
-        pos: Pos,
-    }
-
-    #[test]
-    fn test_ecs() {
-        use bincode::{serde, SizeLimit};
-
-        let mut ecs = Ecs::new();
-
-        let e1 = ecs.make();
-        assert!(ecs.contains(e1));
-
-        assert!(!ecs.pos.contains(e1));
-        ecs.pos.insert(e1, Pos { x: 3, y: 4 });
-        assert_eq!(ecs.pos[e1], Pos { x: 3, y: 4 });
-
-        Desc {
-            name: "Orc".to_string(),
-            icon: 8,
-        }
-        .add_to_ecs(&mut ecs, e1);
-        assert_eq!(ecs.desc[e1].name, "Orc");
-
-        ecs.remove(e1);
-        assert!(!ecs.pos.contains(e1));
-        assert!(!ecs.contains(e1));
-
-        let e2 = ecs.make();
-        assert!(e2 != e1);
-
-        // Use the loadout system to create an entity.
-        let loadout = Loadout::new().c(Desc {
-            name: "Critter".to_string(),
-            icon: 10,
-        });
-
-        // Then instantiate an entity with that form.
-        let e3 = loadout.make(&mut ecs);
-        assert!(ecs.desc[e3].icon == 10);
-
-        // Check that serialization works.
-        let saved = serde::serialize(&ecs, SizeLimit::Infinite).expect("ECS serialization failed");
-        let ecs2 = serde::deserialize::<Ecs>(&saved).expect("ECS deserialization failed");
-        assert!(ecs2.desc[e3].icon == 10);
     }
 }
