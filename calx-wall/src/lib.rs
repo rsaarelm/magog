676211bc4646f9c8edaf0da0661f5c9a1b58ae08
Risto Saarelma <risto.saarelma@iki.fi>
2016-02-14 10:58:17 +0200

    Clean up some obsolete stuff

--- calx-wall/src/lib.rs
+++ calx-wall/src/lib.rs
@@ -10,23 +10,13 @@ extern crate calx_color;
 extern crate calx_layout;
 extern crate calx_cache;
 
-// pub use canvas::{CanvasBuilder, Canvas};
-// pub use canvas::{Image};
 pub use draw_util::DrawUtil;
 // pub use fonter::{Fonter, Align};
 pub use wall::{Wall, Vertex};
 
-// mod canvas;
 mod draw_util;
 // mod fonter;
 mod wall;
-
-/// Drawable images stored in the atlas texture of a Mesh.
-///
-/// By convention, Default Image is assumed to contain a solid-color texel for
-/// drawing solid polygons.
-#[derive(Copy, Clone, PartialEq, Debug, Default)]
-pub struct Image(usize);
 
 /// UI Widget static identifier, unique for a specific site in source code.
 #[derive(Copy, Clone, Debug, PartialEq)]
