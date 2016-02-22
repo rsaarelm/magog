e657d6da8de3403a6e6a0a3c720a758e0ab2b834
Risto Saarelma <risto.saarelma@iki.fi>
2016-02-22 20:50:10 +0200

    Update mapedit to new Calx

--- examples/mapedit.rs
+++ examples/mapedit.rs
@@ -1,11 +1,19 @@
-extern crate calx;
+extern crate cgmath;
+extern crate calx_color;
+extern crate calx_grid;
+extern crate calx_cache;
+extern crate calx_window;
+extern crate calx_wall;
 extern crate content;
 extern crate world;
 extern crate render;
 
-use calx::backend::{Key, Event, MouseButton};
-use calx::{Kernel, V2, color};
-use calx::backend::{WindowBuilder, Canvas, CanvasUtil, CanvasBuilder};
+use cgmath::{Vector2};
+use calx_color::color;
+use calx_cache::{AtlasBuilder};
+use calx_window::{Key, Event, MouseButton, WindowBuilder, Window};
+use calx_grid::Kernel;
+use calx_wall::{Wall, DrawUtil};
 use content::TerrainType;
 use content::Brush;
 use world::{World, Location};
@@ -17,6 +25,27 @@ use render::{Angle, FLOOR_Z, BLOCK_Z};
 struct DrawState {
     pub center: Location,
     pub cursor_loc: Option<Location>,
+}
+
+struct Context {
+    pub window: Window,
+    pub wall: Wall,
+}
+
+impl Context {
+    pub fn new() -> Context {
+        let window = WindowBuilder::new()
+                         .set_title("Mapedit")
+                         .build();
+
+        let mut builder = AtlasBuilder::new();
+        content::Brush::init(&mut builder);
+        let wall = Wall::new(&window.display, builder);
+        Context {
+            window: window,
+            wall: wall,
+        }
+    }
 }
 
 impl DrawState {
@@ -32,18 +61,18 @@ impl DrawState {
         self
     }
 
-    fn draw(&self, ctx: &mut Canvas, w: &World) {
+    fn draw(&self, ctx: &mut Context, w: &World) {
         for pt in cells_on_screen() {
             let screen_pos = chart_to_screen(pt);
             let loc = self.center + pt;
 
-            let k = Kernel::new(|loc| query::terrain(w, loc), loc);
+            let k = Kernel::new(|x, y| query::terrain(w, loc + [x, y]));
             render_terrain(&k, |img, angle, fore, back| {
                 let z = match angle {
                     Angle::Up => FLOOR_Z,
                     _ => BLOCK_Z,
                 };
-                ctx.draw_image(img, screen_pos, z, fore, back)
+                ctx.wall.draw_image(img, screen_pos, z, fore, back)
             });
         }
 
@@ -52,16 +81,16 @@ impl DrawState {
         }
     }
 
-    fn draw_cursor(&self, ctx: &mut Canvas, cursor_loc: Location) {
+    fn draw_cursor(&self, ctx: &mut Context, cursor_loc: Location) {
         if let Some(pt) = self.center.v2_at(cursor_loc) {
             // Draw cursor
             let screen_pos = chart_to_screen(pt);
-            ctx.draw_image(Brush::CursorBottom.get(0),
+            ctx.wall.draw_image(Brush::CursorBottom.get(0),
                            screen_pos,
                            FLOOR_Z,
                            color::RED,
                            color::BLACK);
-            ctx.draw_image(Brush::CursorTop.get(0),
+            ctx.wall.draw_image(Brush::CursorTop.get(0),
                            screen_pos,
                            BLOCK_Z,
                            color::RED,
@@ -72,48 +101,43 @@ impl DrawState {
 }
 
 pub fn main() {
-    let window = WindowBuilder::new()
-                     .set_title("Mapedit")
-                     .build();
-
-    let mut builder = CanvasBuilder::new();
-    content::Brush::init(&mut builder);
-    let mut ctx = builder.build(window);
+    let mut ctx = Context::new();
 
     let mut state = EditState::new();
-    let mut cursor_pos = V2(0, 0);
+    let mut cursor_pos = [0, 0];
 
     loop {
-        let cursor_loc = state.center + view_to_chart(cursor_pos);
+        ctx.window.clear(0x7799DDFF);
+        let cursor_loc = state.center + view_to_chart(Vector2::from(cursor_pos));
 
         DrawState::new(state.center)
             .cursor(cursor_loc)
             .draw(&mut ctx, &state.world);
 
-        for event in ctx.events().into_iter() {
+        for event in ctx.window.events().into_iter() {
             match event {
                 Event::Quit => return,
                 Event::KeyPress(Key::Escape) => return,
 
                 Event::KeyPress(Key::W) => {
-                    state.center = state.center + V2(-1, -1)
+                    state.center = state.center + [-1, -1]
                 }
                 Event::KeyPress(Key::S) => {
-                    state.center = state.center + V2(1, 1)
+                    state.center = state.center + [1, 1]
                 }
                 Event::KeyPress(Key::A) => {
-                    state.center = state.center + V2(-1, 1)
+                    state.center = state.center + [-1, 1]
                 }
                 Event::KeyPress(Key::D) => {
-                    state.center = state.center + V2(1, -1)
+                    state.center = state.center + [1, -1]
                 }
 
-                Event::MouseMove(V2(x, y)) => {
+                Event::MouseMove(pos) => {
                     state.paint(cursor_loc);
 
                     let size = ctx.window.size();
-                    cursor_pos = V2(x as i32 - (size.0 / 2) as i32,
-                                    y as i32 - (size.1 / 2) as i32);
+                    cursor_pos = [pos[0] as i32 - (size[0] / 2) as i32,
+                                  pos[1] as i32 - (size[1] / 2) as i32];
                 }
 
                 Event::MousePress(MouseButton::Left) => {
@@ -133,7 +157,8 @@ pub fn main() {
                 _ => (),
             }
         }
-        ctx.end_frame();
+        ctx.window.display(&mut ctx.wall);
+        ctx.window.end_frame();
     }
 }
 
