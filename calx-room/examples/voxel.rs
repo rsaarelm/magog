04082c7743d7333b95f75a59cb2fdebbf7a52237
Risto Saarelma <risto.saarelma@iki.fi>
2016-03-06 12:07:51 +0200

    Rustfmt

--- calx-room/examples/voxel.rs
+++ calx-room/examples/voxel.rs
@@ -10,23 +10,30 @@ use cgmath::Angle;
 
 /// Cube face.
 #[derive(Copy, Clone)]
-enum Face { East, North, Up, West, South, Down }
+enum Face {
+    East,
+    North,
+    Up,
+    West,
+    South,
+    Down,
+}
 
 impl Face {
     /// Normal vector for the face.
     pub fn normal(&self) -> [f32; 3] {
         match *self {
-            Face::East =>  [ 1.0,  0.0,  0.0],
-            Face::North => [ 0.0,  1.0,  0.0],
-            Face::Up =>    [ 0.0,  0.0,  1.0],
-            Face::West =>  [-1.0,  0.0,  0.0],
-            Face::South => [ 0.0, -1.0,  0.0],
-            Face::Down =>  [ 0.0,  0.0, -1.0],
+            Face::East => [1.0, 0.0, 0.0],
+            Face::North => [0.0, 1.0, 0.0],
+            Face::Up => [0.0, 0.0, 1.0],
+            Face::West => [-1.0, 0.0, 0.0],
+            Face::South => [0.0, -1.0, 0.0],
+            Face::Down => [0.0, 0.0, -1.0],
         }
     }
 
     /// Counter-clockwise vertex list using cube vertex indices.
-#[inline]
+    #[inline]
     pub fn vertices(&self) -> [usize; 4] {
         [[1, 3, 7, 5],
          [2, 6, 7, 3],
@@ -68,13 +75,12 @@ fn cube_vertex(index: usize) -> [f32; 3] {
 #[derive(Copy, Clone)]
 pub struct Vertex {
     pub pos: [f32; 3],
-    pub normal: [f32; 3],
-    //pub color: [f32; 4],
+    pub normal: [f32; 3], // pub color: [f32; 4],
 }
 implement_vertex!(Vertex, pos, normal, color);
 
 struct Voxel {
-    color: [f32; 4]
+    color: [f32; 4],
 }
 
 fn get_voxel(voxelPos: [i32; 3]) -> Option<Voxel> {
@@ -141,17 +147,19 @@ fn main() {
 
         // Camera setup
 
-        let projection: cgmath::Matrix4<f32> = cgmath::PerspectiveFov {
-            fovy: cgmath::Deg::new(90.0).into(),
-            aspect: 380.0 / 640.0, // XXX: Hardcoding
-            near: 1.0,
-            far: 1024.0,
-        }.into();
+        let projection: cgmath::Matrix4<f32> =
+            cgmath::PerspectiveFov {
+                fovy: cgmath::Deg::new(90.0).into(),
+                aspect: 380.0 / 640.0, // XXX: Hardcoding
+                near: 1.0,
+                far: 1024.0,
+            }
+            .into();
 
-        let modelview: cgmath::Matrix4<f32> = cgmath::Matrix4::look_at(
-                cgmath::Point3::new(0.0, -10.0, 10.0),
-                cgmath::Point3::new(0.0, 0.0, 0.0),
-                cgmath::vec3(0.0, 0.0, 1.0));
+        let modelview: cgmath::Matrix4<f32> =
+            cgmath::Matrix4::look_at(cgmath::Point3::new(0.0, -10.0, 10.0),
+                                     cgmath::Point3::new(0.0, 0.0, 0.0),
+                                     cgmath::vec3(0.0, 0.0, 1.0));
 
         // Convert to the format the shader expects.
         let projection: [[f32; 4]; 4] = projection.into();
@@ -168,24 +176,28 @@ fn main() {
             depth: glium::Depth {
                 test: glium::DepthTest::IfLess,
                 write: true,
-                .. Default::default()
+                ..Default::default()
             },
-            .. Default::default()
+            ..Default::default()
         };
 
         // Setup the cube model
-        let faces = [Face::East, Face::North, Face::Up, Face::West, Face::South, Face::Down];
+        let faces = [Face::East,
+                     Face::North,
+                     Face::Up,
+                     Face::West,
+                     Face::South,
+                     Face::Down];
         let mut vertices = Vec::new();
         let mut indices = Vec::new();
         for f in 0..6 {
             normal = faces[f].normal();
             let idx = vertices.len() as u16;
             for v_idx in faces[f].vertices().iter() {
-                vertices.push(
-                        Vertex {
-                            pos: cube_vertex(v_idx),
-                            normal: normal
-                        });
+                vertices.push(Vertex {
+                    pos: cube_vertex(v_idx),
+                    normal: normal,
+                });
 
             }
             indices.push(idx);
@@ -196,7 +208,11 @@ fn main() {
             indices.push(idx + 3);
         }
         let v_buf = glium::VertexBuffer::new(&window.display, &vertices);
-        let i_buf = glium::IndexBuffer::new(&window.display, glium::index::PrimitiveType::TrianglesList, &indices).unwrap();
+        let i_buf =
+            glium::IndexBuffer::new(&window.display,
+                                    glium::index::PrimitiveType::TrianglesList,
+                                    &indices)
+                .unwrap();
 
 
         // Draw the thing
