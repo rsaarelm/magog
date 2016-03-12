2ab47157f385b4f25067a29fb5be30bab90618db
Risto Saarelma <risto.saarelma@iki.fi>
2016-03-12 14:00:05 +0200

    Raytrace demo

--- calx-window/examples/voxel.rs
+++ calx-window/examples/voxel.rs
@@ -16,8 +16,8 @@ struct Frame {
 
 impl Frame {
     pub fn new() -> Frame {
-        let mut pixels = Vec::with_capacity(640 * 360);
-        pixels.resize(640 * 360, 0x00000000);
+        let mut pixels = Vec::with_capacity(320 * 180);
+        pixels.resize(320 * 180, 0x00000000);
         Frame {
             pixels: pixels
         }
@@ -26,12 +26,12 @@ impl Frame {
 
 
     pub fn blit(self, window: &mut Window) {
-        window.set_frame(glium::texture::RawImage2d::from_raw_rgba(self.pixels, (640, 360)));
+        window.set_frame(glium::texture::RawImage2d::from_raw_rgba(self.pixels, (320, 180)));
     }
 }
 
 fn main() {
-    let mut window = WindowBuilder::new().set_title("Voxel demo").build();
+    let mut window = WindowBuilder::new().set_size([320, 180]).set_title("Voxel demo").build();
     let mut tick = 0;
 
     loop {
@@ -50,36 +50,39 @@ fn main() {
         let projection = cgmath::Matrix4::from(
             cgmath::PerspectiveFov {
                 fovy: cgmath::deg(45.0),
-                aspect: 640.0 / 380.0, // XXX: Hardcoding
+                aspect: 16.0 / 9.0, // XXX: Hardcoding
                 near: 0.1,
                 far: 1024.0,
             });
 
+        /*
         let a = (tick as f32) / 96.0;
+        let eye =  cgmath::Point3::new(15.0 * a.sin(),
+            -15.0 * a.cos(),
+            0.0);
+            */
+        let eye = cgmath::Point3::new(0.0, -20.0, 0.0);
         let modelview: cgmath::Matrix4<f32> =
-            cgmath::Matrix4::look_at(cgmath::Point3::new(15.0 * a.sin() +
-                                                         16.0,
-                                                         15.0 * a.cos() +
-                                                         16.0,
-                                                         16.0),
-                                     cgmath::Point3::new(0.0, 0.0, 0.0),
-                                     cgmath::vec3(0.0, 0.0, 1.0));
+            cgmath::Matrix4::look_at(
+                    eye,
+                    cgmath::Point3::new(0.0, 0.0, 0.0),
+                    cgmath::vec3(0.0, 0.0, 1.0));
 
         let mut frame = Frame::new();
 
         let orb = collision::Sphere {
-            center: cgmath::Point3::new(0.0, 20.0, 0.0),
+            center: cgmath::Point3::new(0.0, 0.0, 0.0),
             radius: 1.0,
         };
 
-        for y in 0..360 {
-            for x in 0..640 {
-                let u = (x as f32 - 320.0) / 320.0;
-                let v = (y as f32 - 180.0) / 180.0;
+        for y in 0..180 {
+            for x in 0..320 {
+                let u = (x as f32 - 160.0) / 160.0;
+                let v = -(y as f32 - 90.0) / 90.0;
 
                 let ray = collision::Ray::new(
-                        cgmath::Point3::new(0.0, 0.0, 0.0),
-                        projection./*mul_m(&modelview).*/mul_v(cgmath::vec4(u, 1.0, v, 1.0)).truncate().normalize());
+                        eye,
+                        projection.mul_m(&modelview).mul_v(cgmath::vec4(u, v, 1.0, 0.0)).truncate().normalize());
 
                 if let Some(p) = (orb, ray).intersection() {
                     let normal = p.sub_p(orb.center).normalize();
@@ -88,10 +91,8 @@ fn main() {
                         n = 0.1;
                     }
                     let c: SRgba = Rgba::new(n, n, 0.0, 0.0).into();
-                    frame.pixels[x + 640 * y] = ((c.a as u32) << 24) + ((c.b as u32) << 16) + ((c.g as u32) << 8) + c.r as u32;
+                    frame.pixels[x + 320 * y] = ((c.a as u32) << 24) + ((c.b as u32) << 16) + ((c.g as u32) << 8) + c.r as u32;
                 }
-
-                //println!("{:?}", projection * cgmath::vec4(u, 1.0, v, 1.0));
             }
         }
 
