bf2e6cf01927a5a7664a0f59833dd9c160033bdb
Risto Saarelma <risto.saarelma@iki.fi>
2016-07-03 15:49:17 +0300

    Start sketching up Vitral-based main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ extern crate glium;
 extern crate image;
 extern crate vitral;
 
+use euclid::Point2D;
+
+mod backend;
+
+type Color = [f32; 4];
+
+type ImageRef = usize;
+
+type Splat = Vec<(ImageRef, Point2D<f32>, Color)>;
+
 pub fn main() {
     println!("Hello, world!");
 }
