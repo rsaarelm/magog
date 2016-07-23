6cbfdc77faaa1a496154baf31f1c009f4d0ff986
Risto Saarelma <risto.saarelma@iki.fi>
2016-07-23 11:44:19 +0300

    Activate more world crate stuff

--- world/src/lib.rs
+++ world/src/lib.rs
@@ -1,6 +1,16 @@
+#![feature(custom_derive, plugin)]
+#![plugin(serde_macros)]
+
+extern crate rand;
+extern crate bincode;
+extern crate serde;
+extern crate vec_map;
+extern crate cgmath;
 extern crate image;
 extern crate euclid;
 extern crate vitral;
+extern crate calx_alg;
+extern crate calx_grid;
 extern crate calx_color;
 #[macro_use]
 extern crate calx_ecs;
@@ -8,6 +18,14 @@ extern crate calx_ecs;
 extern crate calx_resource;
 
 mod brush;
-pub mod terrain;
-
 pub use brush::{Brush, BrushBuilder, Color, Frame, ImageRef, Splat};
+
+pub mod item;
+
+mod location;
+pub use location::{Location, Chart, Unchart};
+
+mod location_set;
+mod spatial;
+
+pub mod terrain;
