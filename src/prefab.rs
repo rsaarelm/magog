4c443cd77b38deb88591f811b64e4ede8b82383f
Risto Saarelma <risto.saarelma@iki.fi>
2017-12-16 13:08:25 +0200

    Unit test to check if we need to keep correcting euclid behavior

--- src/prefab.rs
+++ src/prefab.rs
@@ -507,4 +507,16 @@ mod test {
         assert_eq!([0, 0], MinimapSpace::project([0, 1]));
         assert_eq!([0, 0], MinimapSpace::project([1, 1]));
     }
+
+    #[test]
+    fn test_rect_from_points_behavior() {
+        // Currently euclid's Rect::from_points has the surprising behavior that the right and
+        // bottom parts of the point cloud are not included in the rect. The prefab code
+        // compensates for this. Should euclid ever change the API, the compensation would be
+        // wrong, so this test will break. If it does, remove the size adjust found on line 399
+        // when this comment was written.
+        use euclid::{point2, Rect};
+        let rect = Rect::from_points(&[point2(5i32, 5i32)]);
+        assert!(!rect.contains(&point2(5, 5)));
+    }
 }
