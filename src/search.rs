a1b19c894631c23705cd1d3fc7cec30cd9d5b2f8
Risto Saarelma <risto.saarelma@iki.fi>
2017-12-30 16:27:50 +0200

    New A* pather

--- src/search.rs
+++ src/search.rs
@@ -1,8 +1,7 @@
 use num::{One, Zero};
 use num::traits::Num;
-use std::collections::BTreeMap;
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::cmp::Ordering;
+use std::collections::{BTreeMap, BinaryHeap, HashMap, HashSet};
 use std::hash::Hash;
 
 /// A node in a graph with a regular grid.
@@ -72,8 +71,8 @@ impl<N: GridNode> Dijkstra<N> {
     }
 }
 
-/// Find a path between two points using the A* algorithm.
-pub fn astar_path_with<N, F, T>(metric: F, from: N, to: N, mut limit: u32) -> Option<Vec<N>>
+/// Find a path between two grid points using the A* algorithm.
+pub fn astar_grid<N, F, T>(metric: F, from: N, to: N, mut limit: u32) -> Option<Vec<N>>
 where
     N: GridNode,
     F: Fn(&N, &N) -> T,
@@ -152,12 +151,96 @@ where
     None
 }
 
+/// Find A* path in freeform graph.
+///
+/// The `neighbors` function returns neighboring nodes and their estimated distance from the goal.
+/// The search will treat any node whose distance is zero as a goal and return a path leading to
+/// it.
+pub fn astar_path<'a, N, F>(start: N, end: &N, neighbors: F) -> Option<Vec<N>>
+where
+    N: Eq + Hash + Clone,
+    F: Fn(&N) -> Vec<(N, f32)>,
+{
+    #[derive(Eq, PartialEq)]
+    struct MetricNode<N> {
+        value: u32,
+        item: N,
+        come_from: Option<N>,
+    }
+    impl<N: Eq> Ord for MetricNode<N> {
+        fn cmp(&self, other: &Self) -> Ordering { self.value.cmp(&other.value) }
+    }
+    impl<N: Eq> PartialOrd for MetricNode<N> {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> { Some(self.cmp(other)) }
+    }
+
+    fn node<N: Eq>(item: N, dist: f32, come_from: Option<N>) -> MetricNode<N> {
+        debug_assert!(dist >= 0.0);
+        // Convert dist to integers so we can push MetricNodes into BinaryHeap that expects Ord.
+        // The trick here is that non-negative IEEE 754 floats have the same ordering as their
+        // binary representations interpreted as integers.
+        //
+        // Also flip the sign on the value, shorter distance means bigger value, since BinaryHeap
+        // returns the largest item first.
+        let value = ::std::u32::MAX - dist.to_bits();
+        MetricNode {
+            item,
+            value,
+            come_from,
+        }
+    }
+
+    let mut come_from = HashMap::new();
+
+    let mut open = BinaryHeap::new();
+    open.push(node(start.clone(), ::std::f32::MAX, None));
+
+    // Find shortest path.
+    let mut goal = loop {
+        if let Some(closest) = open.pop() {
+            debug_assert!(!come_from.contains_key(&closest.item));
+
+            if let Some(from) = closest.come_from {
+                come_from.insert(closest.item.clone(), from);
+            }
+
+            if &closest.item == end {
+                break Some(closest.item);
+            }
+
+            for (item, dist) in neighbors(&closest.item) {
+                let already_seen = come_from.contains_key(&item) || item == start;
+                if already_seen {
+                    continue;
+                }
+                open.push(node(item, dist, Some(closest.item.clone())));
+            }
+        } else {
+            break None;
+        }
+    };
+
+    // Extract path from the graph structure.
+    let mut path = Vec::new();
+    while let Some(x) = goal {
+        goal = come_from.remove(&x);
+        path.push(x);
+    }
+    path.reverse();
+
+    if path.is_empty() {
+        None
+    } else {
+        Some(path)
+    }
+}
+
 #[cfg(test)]
 mod test {
-    #[test]
-    fn test_astar() {
-        use super::{astar_path_with, GridNode};
+    use super::*;
 
+    #[test]
+    fn test_old_astar() {
         #[derive(PartialEq, Eq, Clone, Hash, PartialOrd, Ord)]
         struct V([i32; 2]);
 
@@ -172,7 +255,7 @@ mod test {
             }
         }
 
-        let path = astar_path_with(
+        let path = astar_grid(
             |a, b| (a.0[0] - b.0[0]).abs() + (a.0[1] - b.0[1]).abs(),
             V([1, 1]),
             V([10, 10]),
@@ -180,5 +263,26 @@ mod test {
         ).unwrap();
         assert!(path[0] == V([1, 1]));
         assert!(path[path.len() - 1] == V([10, 10]));
+    }
+
+    #[test]
+    fn test_astar() {
+        fn neighbors(origin: i32, &x: &i32) -> Vec<(i32, f32)> {
+            let mut ret = Vec::with_capacity(2);
+            for i in &[-1, 1] {
+                let x = x + i;
+                ret.push((x, (x - origin).abs() as f32));
+            }
+            ret
+        }
+
+        assert_eq!(Some(vec![8]), astar_path(8, &8, |_| Vec::new()));
+        assert_eq!(None, astar_path(8, &12, |_| Vec::new()));
+
+        assert_eq!(Some(vec![8]), astar_path(8, &8, |x| neighbors(8, x)));
+        assert_eq!(
+            Some(vec![8, 9, 10, 11, 12]),
+            astar_path(8, &12, |x| neighbors(8, x))
+        );
     }
 }
