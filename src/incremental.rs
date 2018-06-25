1236823eb68dd5fe4aa0837484c25ae2f9d8a03f
Risto Saarelma <risto.saarelma@iki.fi>
2018-06-25 07:59:08 +0300

    Remove error from main Incremental interface

--- src/incremental.rs
+++ src/incremental.rs
@@ -8,11 +8,34 @@ use std::ops::Deref;
 pub trait Incremental: Sized {
     /// Incoming event type.
     type Event;
+
+    /// Try to update the object with an event.
+    fn update(self, e: &Self::Event) -> Self;
+}
+
+/// An incremental object that can fail when updated.
+///
+/// Use the `Incremental` implementation on a `Result` type with `FailingIncremental` value to use
+/// this.
+pub trait FailingIncremental: Sized {
+    /// Incoming event type.
+    type Event;
     /// Error in case an event couldn't be handled.
     type Error: Debug;
 
     /// Try to update the object with an event.
     fn update(self, e: &Self::Event) -> Result<Self, Self::Error>;
+}
+
+impl<T: FailingIncremental> Incremental for Result<T, T::Error> {
+    type Event = T::Event;
+
+    fn update(self, e: &Self::Event) -> Self {
+        match self {
+            Ok(state) => state.update(e),
+            err => err,
+        }
+    }
 }
 
 /// Handle that stores an `Incremental` object and the event sequence used to build it.
@@ -23,6 +46,7 @@ pub struct IncrementalState<T: Incremental + Default> {
     state: T,
 }
 
+// Implemented manually instead of derived so that T::Event doesn't need to implement Default.
 impl<T: Incremental + Default> Default for IncrementalState<T> {
     fn default() -> Self {
         IncrementalState {
@@ -33,54 +57,26 @@ impl<T: Incremental + Default> Default for IncrementalState<T> {
 }
 
 impl<T: Incremental + Default> IncrementalState<T> {
-    pub fn push(&mut self, e: T::Event) -> Result<(), T::Error> {
-        match self.update(&e) {
-            Ok(()) => {
-                self.log.push(e);
-                Ok(())
-            }
-            Err(e) => {
-                // Uh-oh. Update crapped out and state just got eaten. Have to replay to get back
-                // to where we were. XXX: This can be very expensive.
-                //
-                // (The state doesn't have a &mut self update method because you can't really
-                // enforce the implication that an update that results an error shouldn't change
-                // the object in any way. Maybe instead of automatically running the replay a
-                // failed IncrementalState::push should return a core dump object that
-                // has the log up to the point of failure, and the failing event. A new state could
-                // then be constructed from the dump.)
-                self.replay();
-                Err(e)
-            }
-        }
+    pub fn push(&mut self, e: T::Event) {
+        // XXX: Need to get state away from borrow checker for updating, is there a better way than
+        // ugly unsafe tricks?
+        let state = mem::replace(&mut self.state, unsafe { mem::uninitialized() }).update(&e);
+        self.state = state;
+        self.log.push(e);
     }
 
     pub fn pop(&mut self) -> Option<T::Event> {
+        // TODO: Way to pop several items without O(n) delay for each.
         let ret = self.log.pop();
         self.replay();
         ret
-    }
-
-    fn update(&mut self, e: &T::Event) -> Result<(), T::Error> {
-        let mut current_state = Default::default();
-        mem::swap(&mut current_state, &mut self.state);
-
-        match current_state.update(&e) {
-            Ok(s) => {
-                self.state = s;
-                Ok(())
-            }
-            Err(e) => Err(e),
-        }
     }
 
     fn replay(&mut self) {
         let mut state: T = Default::default();
 
         for e in &self.log {
-            // We already verified the update once in `push`, so if it fails now that's a bug
-            // in the state type and causes a panic here.
-            state = state.update(e).unwrap();
+            state = state.update(e);
         }
 
         self.state = state;
@@ -103,13 +99,11 @@ impl<'a, T: Incremental<Event = impl Deserialize<'a>> + Default> Deserialize<'a>
     for IncrementalState<T>
 {
     fn deserialize<D: serde::Deserializer<'a>>(d: D) -> Result<Self, D::Error> {
-        let mut ret: IncrementalState<T> = Default::default();
-        for e in Vec::deserialize(d)?.into_iter() {
-            match ret.push(e) {
-                Ok(()) => {}
-                Err(e) => return Err(D::Error::custom(format!("{:?}", e))),
-            }
-        }
+        let mut ret: Self = IncrementalState {
+            log: Vec::deserialize(d)?,
+            state: Default::default(),
+        };
+        ret.replay();
         Ok(ret)
     }
 }
