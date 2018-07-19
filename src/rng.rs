e1f8ebdd84c19f9ca8d45ef184decfb96ae7467c
Risto Saarelma <risto.saarelma@iki.fi>
2018-07-19 15:29:13 +0300

    Rng helpers for picking single values

--- src/rng.rs
+++ src/rng.rs
@@ -1,4 +1,4 @@
-use rand::{Rng, SeedableRng, XorShiftRng};
+use rand::{seq, Rng, SeedableRng, XorShiftRng};
 use std::hash::Hash;
 use vec_map::VecMap;
 use Deciban;
@@ -32,6 +32,15 @@ pub trait RngExt {
     /// Return true with the probability corresponding to the log odds with
     /// the given deciban value.
     fn with_log_odds(&mut self, db: Deciban) -> bool;
+
+    /// Pick a single random element from a slice.
+    fn pick_slice_ref<'a, U>(&mut self, slice: &'a [U]) -> Option<&'a U>;
+
+    /// Pick a single random element from a slice.
+    fn pick_slice<'a, U: Clone>(&mut self, slice: &'a [U]) -> Option<U>;
+
+    /// Pick a single random element from an iterator
+    fn pick_iter<U>(&mut self, iter: impl IntoIterator<Item = U>) -> Option<U>;
 }
 
 impl<T: Rng> RngExt for T {
@@ -42,6 +51,19 @@ impl<T: Rng> RngExt for T {
     fn with_chance(&mut self, p: f32) -> bool { self.gen_range(0.0, 1.0) < p }
 
     fn with_log_odds(&mut self, db: Deciban) -> bool { db > self.gen::<Deciban>() }
+
+    fn pick_slice_ref<'a, U>(&mut self, slice: &'a [U]) -> Option<&'a U> {
+        seq::sample_slice_ref(self, slice, 1).pop()
+    }
+
+    fn pick_slice<U: Clone>(&mut self, slice: &[U]) -> Option<U> {
+        seq::sample_slice(self, slice, 1).pop()
+    }
+
+    /// Pick a single random element from an iterator
+    fn pick_iter<U>(&mut self, iter: impl IntoIterator<Item = U>) -> Option<U> {
+        seq::sample_iter(self, iter, 1).unwrap_or(Vec::new()).pop()
+    }
 }
 
 /// Lazily evaluated random permutation.
