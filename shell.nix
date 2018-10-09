eed59662f54a086c20822f49e5202f4d0e6faaa0
Risto Saarelma <risto.saarelma@iki.fi>
2018-10-09 20:53:11 +0300

    Update to NixOS 18.09

--- shell.nix
+++ shell.nix
@@ -12,6 +12,12 @@ stdenv.mkDerivation {
   # XXX: This isn't the proper Nix way to do setup
   # TODO: Support cross-compilation to target x86_64-pc-windows-gnu
   shellHook = ''
+    # Load the GL and X11 stuff the graphics app wants to link dynamically to
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
+      with pkgs.xlibs; lib.makeLibraryPath
+      [ pkgs.libGL libX11 libXcursor libXxf86vm libXi libXrandr ]
+    }"
+
     rustup install nightly
     rustup default nightly
     rustup update
@@ -28,13 +34,4 @@ stdenv.mkDerivation {
   # Set Environment Variables
   RUST_BACKTRACE = 1;
   RUST_LOG = "info";
-
-  # Stuff needed to run Glium programs.
-  LD_LIBRARY_PATH = with pkgs.xlibs;
-     "${pkgs.mesa}/lib:"
-    +"${libX11}/lib:"
-    +"${libXcursor}/lib:"
-    +"${libXxf86vm}/lib:"
-    +"${libXi}/lib:"
-    +"${libXrandr}/lib";
 }
