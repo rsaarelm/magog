57da98cc250de84554ba38d4e8096ddb05e2b51a
Risto Saarelma <risto.saarelma@iki.fi>
2019-02-25 17:34:38 +0200

    build: Use stable Rust

--- shell.nix
+++ shell.nix
@@ -12,23 +12,23 @@ stdenv.mkDerivation {
   # XXX: This isn't the proper Nix way to do setup
   # TODO: Support cross-compilation to target x86_64-pc-windows-gnu
   shellHook = ''
+    export RUST_BACKTRACE=1
+
     # Load the GL and X11 stuff the graphics app wants to link dynamically to
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
       with pkgs.xlibs; lib.makeLibraryPath
       [ pkgs.libGL libX11 libXcursor libXxf86vm libXi libXrandr ]
     }"
 
-    rustup install nightly
-    rustup default nightly
+    rustup install stable
+    rustup default stable
     rustup update
 
     rustup component add rls-preview rust-analysis rust-src
     rustup component add rls-preview rust-analysis rust-src --toolchain nightly
     rustup component add rustfmt-preview clippy-preview --toolchain nightly
 
-    # FIXME: These run into some linker problem when run from shellHook
-    # They can be installed manually once in the shell though.
-    # cargo install cargo-outdated
+    NIX_ENFORCE_PURITY=0 cargo install cargo-outdated
   '';
 
   # Set Environment Variables
