3499725c27fb0a229d616a0ecfc33985f2d549c9
Risto Saarelma <risto.saarelma@iki.fi>
2019-04-05 13:45:36 +0300

    Remove this, currently there's no working nix package build

--- default.nix
+++
@@ -1,6 +0,0 @@
-let
-   pkgs = import <nixpkgs> {};
-in pkgs.stdenv.mkDerivation rec {
-  name = "glutin-env";
-  LD_LIBRARY_PATH = with pkgs.xlibs; "${pkgs.mesa}/lib:${libX11}/lib:${libXcursor}/lib:${libXxf86vm}/lib:${libXi}/lib:${libXrandr}/lib";
-}
