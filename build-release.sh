58b37f1cea1c033d12d60eee66c8f14934d3236b
Risto Saarelma <risto.saarelma@iki.fi>
2019-05-22 08:08:22 +0300

    Touch up the release build script

--- build-release.sh
+++ build-release.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# XXX: We want some extra-slow binary size optimizing flags for the release
+# build. Rust's debug builds can be very slow so we also want faster-building
+# release builds during development, and cargo does not support more profiles,
+# so this will generate a temporary cargo file that has the additional flags
+# set and build everything using it.
+
 WORK_DIR=`mktemp -d`
 git clone . $WORK_DIR
 pushd $WORK_DIR
@@ -11,6 +17,6 @@ codegen-units = 1
 panic = 'abort'
 EOF
 
-cargo +nightly build --release
+cargo +stable build --release
 strip -s target/release/magog
-readlink -f target/release/magog
+echo "Built release binary `readlink -f target/release/magog`"
